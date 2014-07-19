c8e731894183cbf9407640a065bf1bdbd47790de
Julien Vaslet <julien.vaslet@gmail.com>
2014-07-19 17:41:25 +0200

    Camera class, test app ok

--- apps/004_Camera.cpp
+++ apps/004_Camera.cpp
@@ -28,18 +28,86 @@ int main( int argc, char ** argv )
 	SDL_Event lastEvent;
 	unsigned int lastDrawTicks = 0;
 	bool customMatrix = false;
+	
+	float yRotation = 0.0f;
+	unsigned int lastRotateTicks = 0;
+	
+	bool cameraOrtho = false;
+	bool cameraPerspective = true;
+	bool cameraFrustum = false;
 
 	// Generating points
 	vector<Point> m_points;
-	m_points.push_back( Point( -0.5f, -0.5f, -2.0f ) );
-	m_points.push_back( Point( 0.5f, -0.5f, -2.0f ) );
-	m_points.push_back( Point( 0.0f, 0.5f, -2.0f ) );
+	m_points.push_back( Point( -0.5f, 0.0f, 0.5f ) );
+	m_points.push_back( Point( 0.5f, 0.0f, 0.5f ) );
+	m_points.push_back( Point( 0.5f, 1.0f, 0.5f ) );
+	m_points.push_back( Point( -0.5f, 1.0f, 0.5f ) );
+	m_points.push_back( Point( -0.5f, 1.0f, -0.5f ) );
+	m_points.push_back( Point( -0.5f, 0.0f, -0.5f ) );
+	m_points.push_back( Point( 0.5f, 0.0f, -0.5f ) );
+	m_points.push_back( Point( 0.5f, 1.0f, -0.5f ) );
+	
+	// Generating indexes
+	vector<unsigned int> m_indices;
+	
+	// Front face
+	m_indices.push_back( 0 );
+	m_indices.push_back( 1 );
+	m_indices.push_back( 2 );
+	m_indices.push_back( 0 );
+	m_indices.push_back( 2 );
+	m_indices.push_back( 3 );
+	
+	// Top face
+	m_indices.push_back( 3 );
+	m_indices.push_back( 2 );
+	m_indices.push_back( 4 );
+	m_indices.push_back( 2 );
+	m_indices.push_back( 7 );
+	m_indices.push_back( 4 );
+	
+	// Right face
+	m_indices.push_back( 1 );
+	m_indices.push_back( 6 );
+	m_indices.push_back( 2 );
+	m_indices.push_back( 6 );
+	m_indices.push_back( 7 );
+	m_indices.push_back( 2 );
+	
+	// Left face
+	m_indices.push_back( 0 );
+	m_indices.push_back( 3 );
+	m_indices.push_back( 4 );
+	m_indices.push_back( 4 );
+	m_indices.push_back( 5 );
+	m_indices.push_back( 0 );
+	
+	// Back face
+	m_indices.push_back( 4 );
+	m_indices.push_back( 6 );
+	m_indices.push_back( 5 );
+	m_indices.push_back( 4 );
+	m_indices.push_back( 7 );
+	m_indices.push_back( 6 );
+	
+	// Bottom face
+	m_indices.push_back( 0 );
+	m_indices.push_back( 5 );
+	m_indices.push_back( 6 );
+	m_indices.push_back( 6 );
+	m_indices.push_back( 1 );
+	m_indices.push_back( 0 );
 	
 	// Generating colors
 	vector<Color> m_colors;
 	m_colors.push_back( Color( "ff0000" ) );
 	m_colors.push_back( Color( "00ff00" ) );
 	m_colors.push_back( Color( "0000ff" ) );
+	m_colors.push_back( Color( "ffff00" ) );
+	m_colors.push_back( Color( "00ffff" ) );
+	m_colors.push_back( Color( "ff00ff" ) );
+	m_colors.push_back( Color( "000000" ) );
+	m_colors.push_back( Color( "ffffff" ) );
 	
 	ArrayBufferObject * vbo = new ArrayBufferObject();
 	vbo->setData( m_points );
@@ -48,6 +116,15 @@ int main( int argc, char ** argv )
 	cbo->setData( m_colors );
 	
 	Camera camera;
+	camera.getEye().moveTo( 0.0f, 1.25f, 4.0f );
+	camera.getCenter().moveTo( 0.0f, 0.0f, 0.0f );
+	
+	glEnable( GL_DEPTH_TEST );
+	glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+	
+	cout << "Press [SPACE] to switch between custom and OpenGL camera and matrices management." << endl;
+	cout << "Press [F1], [F2], [F3] to switch between, respectively, perspective view, orthogonal view and frustum view." << endl;
+	cout << "Press arrow keys to move the box." << endl;
 
 	while( running )
 	{
@@ -75,6 +152,55 @@ int main( int argc, char ** argv )
 		            	else
 		            		cout << "Using OpenGL matrices." << endl;
 		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_F1 )
+		            {
+		            	cout << "Using perspective view" << endl;
+						cameraOrtho = false;
+						cameraPerspective = true;
+						cameraFrustum = false;
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_F2 )
+		            {
+		            	cout << "Using orthogonal view" << endl;
+						cameraOrtho = true;
+						cameraPerspective = false;
+						cameraFrustum = false;
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_F3 )
+		            {
+		            	cout << "Using frustum view" << endl;
+						cameraOrtho = false;
+						cameraPerspective = false;
+						cameraFrustum = true;
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_LEFT )
+		            {
+		            	camera.getCenter().moveBy( -0.1f, 0.0f, 0.0f );
+		            	
+		            	if( camera.getCenter().getX() < -5.0f )
+		            		camera.getCenter().setX( -5.0f );
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_RIGHT ) 
+		            {
+		            	camera.getCenter().moveBy( 0.1f, 0.0f, 0.0f );
+		            	
+		            	if( camera.getCenter().getX() > 5.0f )
+		            		camera.getCenter().setX( 5.0f );
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_DOWN ) 
+		            {
+		            	camera.getCenter().moveBy( 0.0f, -0.1f, 0.0f );
+		            	
+		            	if( camera.getCenter().getY() < -2.0f )
+		            		camera.getCenter().setY( -2.0f );
+		            }
+		            else if( lastEvent.key.keysym.sym == SDLK_UP )
+		            {
+		            	camera.getCenter().moveBy( 0.0f, 0.1f, 0.0f );
+		            	
+		            	if( camera.getCenter().getY() > 5.0f )
+		            		camera.getCenter().setY( 5.0f );
+		            }
 
 					break;
 				}
@@ -83,23 +209,65 @@ int main( int argc, char ** argv )
 		
 		unsigned int ticks = SDL_GetTicks();
 		
+		if( ticks - lastRotateTicks > 20 )
+		{
+			yRotation = static_cast<float>( ( static_cast<int>( yRotation ) + 1 ) % 360 );
+			lastRotateTicks = ticks;
+		}
+		
 		if( ticks - lastDrawTicks > 15 )
 		{
 			Screen::get()->clear();
 			
 			if( customMatrix )
 			{
-				camera.setPerspective( 45.0f, 800.0f / 600.0f, 1.0f, 100.0f );
+				if( cameraPerspective )
+				{
+					camera.setPerspective( 45.0f, 800.0f / 600.0f, 1.0f, 100.0f );
+				}
+				else if( cameraOrtho )
+				{
+					camera.setOrthogonal( -2.5f, 2.5f, -1.88f, 1.88f, 1.0f, 100.0f );
+				}
+				else if( cameraFrustum )
+				{
+					camera.setFrustum( -1.25f, 1.25f, -0.89f, 0.89f, 1.0f, 100.0f );
+				}
 			}
 			else
 			{
 				glMatrixMode( GL_PROJECTION );
 				glLoadIdentity();
-				gluPerspective( 45.0f, 800.0f / 600.0f, 1.0f, 100.0f );
+				
+				if( cameraPerspective )
+				{
+					gluPerspective( 45.0f, 800.0f / 600.0f, 1.0f, 100.0f );
+				}
+				else if( cameraOrtho )
+				{
+					glOrtho( -2.5f, 2.5f, -1.88f, 1.88f, 1.0f, 100.0f );
+				}
+				else if( cameraFrustum )
+				{
+					glFrustum( -1.25f, 1.25f, -0.89f, 0.89f, 1.0f, 100.0f );
+				}
 			}
 			
 			glMatrixMode( GL_MODELVIEW );
 			camera.look();
+			
+			if( customMatrix )
+			{
+				glMultMatrixf( Matrix::translation( camera.getCenter().getX(), camera.getCenter().getY(), camera.getCenter().getZ() ).get() );
+				glMultMatrixf( Matrix::rotationY( yRotation ).get() );
+			}
+			else
+			{
+				glTranslatef( camera.getCenter().getX(), camera.getCenter().getY(), camera.getCenter().getZ() );
+				glRotatef( yRotation, 0.0f, 1.0f, 0.0 );
+			}
+			
+			glColor3f( 1.0f, 1.0f, 1.0f );
 			
 			glBindBuffer( GL_ARRAY_BUFFER, cbo->getBuffer() );
 			glColorPointer( 4, GL_FLOAT, 0, 0 );
@@ -108,11 +276,11 @@ int main( int argc, char ** argv )
 			glVertexPointer( 3, GL_FLOAT, 0, 0 );
 			
 			// Render with Vertex Arrays
-			glEnableClientState( GL_VERTEX_ARRAY );
 			glEnableClientState( GL_COLOR_ARRAY );
-			glDrawArrays( GL_TRIANGLES, 0, 3 );
-			glDisableClientState( GL_COLOR_ARRAY );
+			glEnableClientState( GL_VERTEX_ARRAY );
+			glDrawElements( GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, m_indices.data() );
 			glDisableClientState( GL_VERTEX_ARRAY );
+			glDisableClientState( GL_COLOR_ARRAY );
 			
 			Screen::get()->render();
 			
--- source/opengl/Screen.cpp
+++ source/opengl/Screen.cpp
@@ -130,7 +130,7 @@ namespace opengl
 	{
 		//this->resetRenderColor();
 		//glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		glLoadIdentity();
 	}
 
