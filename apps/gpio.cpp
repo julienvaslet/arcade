902db244c076e8320a92f5ee6f6b31b37a7c8163
Julien Vaslet <julien.vaslet@gmail.com>
2014-11-25 23:53:07 +0100

    GPIO class must be tested on raspberry pi

--- apps/gpio.cpp
+++ apps/gpio.cpp
@@ -1,6 +1,10 @@
 #include <tools/logger/Stdout.h>
+#include <tools/gpio/GPIO.h>
+
+#include <unistd.h>
 
 using namespace tools::logger;
+using namespace tools::gpio;
 
 int main( int argc, char ** argv )
 {
@@ -8,12 +12,36 @@ int main( int argc, char ** argv )
 	new Stdout( "stdout", true );
 	
 	bool running = true;
+	int times = 50;
+	
+	GPIO::initialize();
+	GPIO::get()->open( 21, GPIO::In );
+
+	bool pushed = false;
 
 	while( running )
 	{
-		break;
+		bool status = GPIO::get()->read( 21 );
+		
+		if( status != pushed )
+		{
+			pushed = status;
+			
+			if( pushed )
+				Logger::get() << "Button pushed." << Logger::endl;
+			else
+				Logger::get() << "Button released." << Logger::endl;
+		}
+		
+		// Wait 100 ms
+		usleep( 100000 );
+		times--;
+		
+		if( times <= 0 )
+			running = false;
 	}
 	
+	GPIO::destroy();
 	Logger::destroy();
 	
 	return 0;
